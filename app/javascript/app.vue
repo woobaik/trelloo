de52b625b3a59220634661f82f98f931c76f4aec
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-09-09 19:31:21 -0700

    Mockup basic template

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    ?
+    <app-navigation></app-navigation>    
     <app-home></app-home>    
-    <app-home></app-home>    
-
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+// layouts 
+import Navigation from './components/layouts/Navigation.vue'
+import Footer from './components/layouts/Footer.vue'
 
+// homepage
 import Home from './components/Home.vue'
+
 export default {
   components: { 
-    'app-home': Home
+    'app-navigation': Navigation,
+    'app-home': Home,
+    'app-footer': Footer
   }
 }
 </script>
