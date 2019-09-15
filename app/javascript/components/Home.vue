9e776bc19d21704620a99e031ca61befefa405ac
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-09-14 20:40:47 -0700

    Refactored home & card component

--- app/javascript/components/Home.vue
+++ app/javascript/components/Home.vue
@@ -4,33 +4,53 @@
         <button class="add-button">New Card</button>
       </div>
       <div class="my-home">
-        <div class="my-card">
-          <div class="my-card-title">
-            <div>Todo List</div>
-            <div>
-              <i class="material-icons plus-icon">add_circle_outline</i>
-            </div>
-          </div>
-          <div class="my-card-body">
-            <ul class="my-card-list">
-              <li>Install Firetvstick</li>
-              <li>Bring coffee from car</li>    
-            </ul>
-          </div>
-          
-          <div class="my-card-footer">
-            <button class="my-card-footer-delete">Delete Card</button>
-          </div>
-        </div>
-
-       
+        <my-card v-for="card in cards" :key="card.name" :card="card"></my-card>   
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import Card from './layouts/Card.vue'
 
+export default {
+  data: function() {
+    return {
+      cards: [
+        {
+          name: 'Todo List', 
+          lists: [
+            {
+              name: 'Install FireStickTV',
+              completed: false
+            },
+            {
+              name: 'Pay Electric Bill',
+              completed: true
+            }
+          ]
+        },
+
+        {
+          name: 'In Progress',
+          lists: [
+            {
+              name: 'Finish Trello Project',
+              completed: false
+            },
+
+            {
+              name: 'Update My Linked In',
+              complted: true
+            }
+          ]
+        }
+      ]
+    }
+  },
+
+  components: {
+    'my-card': Card
+  }
 }
 </script>
 
@@ -54,6 +74,7 @@ export default {
     border-radius: 5px;
     transition: all 0.3s ease-in-out;
     margin-bottom: 10px;
+    cursor: pointer;
   }
 
   .add-button:hover {
@@ -78,81 +99,5 @@ export default {
     overflow: scroll;
   }
 
-  .my-card {
-    background-color: white;
-    margin-top: 2vh;
-    margin-left: 1vw;
-    width: 32vw;
-    border-radius: 5px;
-    overflow-y:scroll;
-    overflow: scroll;
-    position: relative;
-  }
-
-  .my-card-title {
-    font-family: 'Ubuntu', sans-serif;
-    font-size: 1.5rem;
-    height: 2.2rem;
-    margin: 2rem;
-    text-align: center;
-    display: flex;
-    justify-content: space-between;
-    padding-bottom: 0.7rem;
-    border-bottom: 1px solid lightgray;
-  }
-
-  .plus-icon {
-    color: #0079bf;
-    transition: all 0.2s ease-in-out;
-    cursor: pointer;
-  }
-
-  .plus-icon:hover {
-    color:mediumseagreen;
-    font-size: 2.2rem;
-    
-  }
-
-  .my-card-list {
-    list-style: none;
-    padding: 0;
-    text-align: center;
-  }
-
-  .my-card-list li {
-    font-size: 1rem;
-    font-family: 'Saira Semi Condensed', sans-serif;
-    margin: 0 5% 1% 5%;
-    background-color: #0079bf;
-    color: white;
-    border-radius: 0.5vw;
-    width: 90%;
-    padding: 0.5rem 0;
-    cursor: pointer;
-  }
-
-  .my-card-footer {
-    text-align: center;
-  }
-
-  .my-card-footer-delete {
-    font-size: 1.2rem;
-    background-color: white;
-    color: #ee4056;
-    height: 5vh;
-    width: 10rem;;
-    border-radius: 10rem;
-    transition: all 0.2s ease-in-out;
-    font-family: 'Saira Semi Condensed', sans-serif;
-  }
-
-    .my-card-footer-delete:hover {
-    font-size: 1.2rem;
-    background-color: #ee4056;
-    height: 5vh;
-    width: 10rem;;
-    border-radius: 10rem;
-    color: white;
-  }
 
 </style>
---
+++ app/javascript/components/layouts/Card.vue
@@ -0,0 +1,106 @@
+<template>    
+    <div class="my-card">
+        <div class="my-card-title">
+            <div>{{ card.name }}</div>
+            <div>
+            <i class="material-icons plus-icon">add_circle_outline</i>
+            </div>
+        </div>
+        <div class="my-card-body">
+            <ul class="my-card-list">
+            <li v-for="list in card.lists" :key="list.name"> {{ list.name}}</li>
+            
+            </ul>
+        </div>
+        
+        <div class="my-card-footer">
+            <button class="my-card-footer-delete">Delete Card</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['card']
+}
+</script>
+
+<style scoped>
+
+    .my-card {
+    background-color: rgb(243, 243, 243);
+    margin-top: 2vh;
+    margin-left: 1vw;
+    width: 32vw;
+    border-radius: 5px;
+    overflow-y:scroll;
+    overflow: scroll;
+    position: relative;
+    }
+
+    .my-card-title {    
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 1.5rem;
+        height: 2.2rem;
+        margin: 2rem;
+        text-align: center;
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 0.7rem;
+        border-bottom: 1px solid lightgray;
+  }
+
+  .plus-icon {
+    color: #0079bf;
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  }
+
+  .plus-icon:hover {
+    color:mediumseagreen;
+    font-size: 2.2rem;
+    
+  }
+
+  .my-card-list {
+    list-style: none;
+    padding: 0;
+    text-align: center;
+  }
+
+  .my-card-list li {
+    font-size: 1rem;
+    font-family: 'Saira Semi Condensed', sans-serif;
+    margin: 0 5% 1% 5%;
+    background-color: #0079bf;
+    color: white;
+    border-radius: 0.5vw;
+    width: 90%;
+    padding: 0.5rem 0;
+    cursor: pointer;
+  }
+
+    .my-card-footer {
+    text-align: center;
+  }
+
+  .my-card-footer-delete {
+    font-size: 1.2rem;
+    background-color: white;
+    color: #ee4056;
+    height: 5vh;
+    width: 10rem;;
+    border-radius: 10rem;
+    transition: all 0.2s ease-in-out;
+    font-family: 'Saira Semi Condensed', sans-serif;
+  }
+
+    .my-card-footer-delete:hover {
+    font-size: 1.2rem;
+    background-color: #ee4056;
+    height: 5vh;
+    width: 10rem;;
+    border-radius: 10rem;
+    color: white;
+  }
+</style>
